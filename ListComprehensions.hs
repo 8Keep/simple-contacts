e37078e80e1813c63345751de440893cb43be6e0
Davis Rollman <drollma@hotmail.com>
2018-09-02 23:42:24 -0400

    testing... do not look

---
+++ ListComprehensions.hs
@@ -0,0 +1,5 @@
+multipleOf5 x = [ x | x <- [1..x], (x `mod` 5 == 0))]
+
+multipleOf7Or11 x = [ x | x <- [1..x], ((x `mod` 7 == 0) || (x `mod` 11 == 0))]
+
+multipleOf3And7 x = [ x | x <- [1..x], ((x `mod` 3 == 0) && (x `mod` 7 == 0))]

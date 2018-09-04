f6b89602221fe038e2053bfd03b5e50c181466f0
Hugens25 <42974909+Hugens25@users.noreply.github.com>
2018-09-04 18:22:54 -0400

    cleaned up authenticate.php
    Changed my "messages" to make sure they were producing json code.

--- back-end/testapi/users/authenticate.php
+++ back-end/testapi/users/authenticate.php
@@ -6,27 +6,26 @@ header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-// get database connection
+// include database and object file
 include_once '../config/database.php';
-
-// instantiate contact object
 include_once '../objects/users.php';
 
+// get database connection
 $database = new Database();
 $db = $database->getConnection();
 
-// initialize object
+// instantiate contact object
 $contact = new User($db, "users");
 
-// get name
+// get username and password
 $name=isset($_GET["n"]) ? $_GET["n"] : "";
 $password=isset($_GET["p"]) ? $_GET["p"] : "";
 
-// query contacts
+// query users table to see if username/password exists in database
 $stmt = $contact->authenticate($name, $password);
 $num = $stmt->rowCount();
 
-// check if more than 0 record found
+// check if EXACTLY one record is found. No duplicates are allowed
 if($num == 1){
   echo json_encode(
       array("message" => "Successful Login!")

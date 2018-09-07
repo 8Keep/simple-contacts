44b54a999a2a3b7ad4ad2585bc48e933d6b7f2b2
TheBombTNT <TakeoRyuuken@users.noreply.github.com>
2018-09-07 18:13:08 -0400

    append

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,14 +1,29 @@
+var username;
+
+$( document ).ready(function() {
+    username = getUsername();
+    console.log( "document loaded! checking for username cookie." );
+    if (username == undefined) {
+        console.log("username is undefined");
+        //TODO: Uncomment this in final version
+        //alert("User is not logged in! You will now be redirected to login.");
+        //window.location.href="index.html";
+    }
+    $("#searchbox").val("");
+    search();
+});
 
 function add() {
     var contact = { name: $("#name").val(),
                     phone: $("#phone").val(),
-                    address: $("#address").val() };
+                    address: $("#address").val(),
+                    username: username };
     
-    alert(JSON.stringify(contact));
+    $.post(
+        "/COP4331-Small-Project-master/back-end/testapi/contact/create.php",
+        JSON.stringify(contact),
+        function(result){     });
     
-    $.post("/COP4331-Small-Project-master/back-end/testapi/contact/create.php", JSON.stringify(contact), function(result){
-        alert(result);
-        });
     
     $("#name").val("");
     $("#phone").val("");
@@ -16,7 +31,20 @@ function add() {
 }
 
 function getUsername() {
-    return $.cookie("username");
+    return Cookies.get("username");
+}
+
+function search()
+{
+    var json = "{ \"username\":\"" + username + "\", \"keyword\":\"" + $("#searchbox").val() +  "\" }";
+    
+    alert(json);
+    
+    $.post(
+        "/COP4331-Small-Project-master/back-end/testapi/contact/search.php",
+        json,
+        function(result){     });
+    
 }
 
 function appendText(name, id)

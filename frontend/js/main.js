45aa6feff72d2ffb570aced8c8afa6b0f32ca4ee
Davis Rollman <drollma@hotmail.com>
2018-09-07 14:03:21 -0400

    started contact js

---
+++ frontend/js/main.js
@@ -0,0 +1,56 @@
+$(document).ready(function(){
+    getAllContacts();
+}
+
+function add()
+{
+    
+}
+
+function getAllContacts() {
+    $.getJSON("/search.php"
+}
+
+function search()
+{
+    var srch = document.getElementById("searchbox").value;
+    document.getElementById("colorSearchResult").innerHTML = "";
+    
+    var colorList = document.getElementById("colorList");
+    colorList.innerHTML = "";
+    
+    var jsonPayload = '{"search" : "' + srch + '"}';
+    var url = urlBase + '/SearchColors.' + extension;
+    
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+    try
+    {
+        xhr.onreadystatechange = function() 
+        {
+            if (this.readyState == 4 && this.status == 200) 
+            {
+                hideOrShow( "colorList", true );
+                
+                document.getElementById("colorSearchResult").innerHTML = "Color(s) has been retrieved";
+                var jsonObject = JSON.parse( xhr.responseText );
+                
+                var i;
+                for( i=0; i<jsonObject.results.length; i++ )
+                {
+                    var opt = document.createElement("option");
+                    opt.text = jsonObject.results[i];
+                    opt.value = "";
+                    colorList.options.add(opt);
+                }
+            }
+        };
+        xhr.send(jsonPayload);
+    }
+    catch(err)
+    {
+        document.getElementById("colorSearchResult").innerHTML = err.message;
+    }
+    
+}
